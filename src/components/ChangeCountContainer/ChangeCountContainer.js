61a3c90e1c91a86ef82735cf52e71dcf947575b2
mohammad <mmdshayei78@gmail.com>
2021-08-07 19:04:49 +0430

    third commit for redux

--- src/components/ChangeCountContainer/ChangeCountContainer.js
+++ src/components/ChangeCountContainer/ChangeCountContainer.js
@@ -2,18 +2,26 @@ import React, { useState } from "react";
 import "./ChangeCountContainer.css";
 import { useDispatch, useSelector } from "react-redux";
 import * as actions from "../../store/actions/counter";
-const ChangeCountContainer = (props) => {
-  const dispatch = useDispatch();
-  const detail = useSelector((state) => state.detail);
 
-  const selectSoftware = (software) => {
-    dispatch(actions.selectSoftware(software));
+const ChangeCountContainer = (props) => {
+
+  const dispatch = useDispatch();
+  const counter= useSelector((state) => state.counter);
+
+  const onIncrementHandler = (id) => {
+    dispatch(actions.incrementHandler(id));
   };
+  const onDecrementHandler = (id) => {
+    dispatch(actions.decrementHandler(id));
+  }
+  
   return (
     <div className="change-count-container">
-      <p onClick={() => props.onIncrementHandler(props.id)}>+</p>
-      <p>{props.count !== 0 ? props.count : "zero"}</p>
-      <p onClick={() => props.onDecrementHandler(props.id)}>-</p>
+      {/* <p onClick={() => props.onIncrementHandler(props.id)}>+</p> */}
+      <p onClick={() => onIncrementHandler(props.id)}>+</p>
+      {/* <p>{props.count !== 0 ? props.count : "zero"}</p> */}
+      <p>{counter.dataFood[0].count}</p>
+      <p onClick={() => onDecrementHandler(props.id)}>-</p>
       <p>{props.title} </p>
     </div>
   );
